d9ec5ec6bf5624678ecef3300fb5bf84be7f4bb4
Ye Ja <68594713+jayecc@users.noreply.github.com>
2024-02-07 14:51:26 +0800

    Add files via upload

---
+++ logger.go
@@ -0,0 +1,130 @@
+package lib
+
+import (
+	"context"
+	"go.opentelemetry.io/otel/trace"
+	"io"
+	"log/slog"
+	"runtime"
+	"strings"
+)
+
+type hookHandler func(r slog.Record)
+
+type Hook struct {
+	level  slog.Level
+	handle hookHandler
+}
+
+func NewHook(level slog.Level, handle hookHandler) *Hook {
+	return &Hook{
+		level:  level,
+		handle: handle,
+	}
+}
+
+type hookTurn struct {
+	r slog.Record
+	h hookHandler
+}
+
+type Handler struct {
+	slog.Handler
+	hooks    []*Hook
+	hookChan chan *hookTurn
+}
+
+func ParseLevel(level string) slog.Level {
+	switch strings.ToLower(level) {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
+
+func NewHandler(w io.Writer, opts *slog.HandlerOptions, hooks ...*Hook) *Handler {
+	h := &Handler{
+		Handler:  slog.NewJSONHandler(w, opts),
+		hooks:    hooks,
+		hookChan: make(chan *hookTurn, 1<<10),
+	}
+	if len(hooks) > 0 {
+		go h.hook()
+	}
+	return h
+}
+
+func (h *Handler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.Handler.Enabled(ctx, level)
+}
+
+func (h *Handler) WithGroup(name string) slog.Handler {
+	clone := *h
+	clone.Handler = h.Handler.WithGroup(name)
+	return &clone
+}
+
+func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	clone := *h
+	clone.Handler = h.Handler.WithAttrs(attrs)
+	return &clone
+}
+
+func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
+
+	if !h.Handler.Enabled(ctx, r.Level) {
+		return h.Handler.Handle(ctx, r)
+	}
+	var attrs []slog.Attr
+	spanCtx := trace.SpanContextFromContext(ctx)
+	if spanCtx.HasTraceID() {
+		attrs = append(attrs, slog.String("trace", spanCtx.TraceID().String()))
+	}
+	if spanCtx.HasSpanID() {
+		attrs = append(attrs, slog.String("span", spanCtx.SpanID().String()))
+	}
+	if r.Level == slog.LevelError {
+		fs := runtime.CallersFrames([]uintptr{r.PC})
+		f, _ := fs.Next()
+		attrs = append(attrs, slog.Any("source", slog.Source{Function: f.Function, File: f.File, Line: f.Line}))
+	}
+	if len(attrs) > 0 {
+		r.AddAttrs(attrs...)
+	}
+	for _, hook := range h.hooks {
+		if len(h.hookChan) >= (1<<10)-1 {
+			continue
+		}
+		if hook.level == r.Level {
+			h.hookChan <- &hookTurn{h: hook.handle, r: r}
+		}
+	}
+	return h.Handler.Handle(ctx, r)
+}
+
+func (h *Handler) hook() {
+	for {
+		select {
+		case hook := <-h.hookChan:
+			hook.h(hook.r)
+		}
+	}
+}
+
+//idx := strings.LastIndexByte(f.File, '/')
+//if idx > 0 {
+//	idx = strings.LastIndexByte(f.File[:idx], '/')
+//	if idx > 0 {
+//		f.File = f.File[idx+1:]
+//	}
+//}
+//buf := make([]byte, 64<<10)
+//n := runtime.Stack(buf, false)
+//buf = buf[:n]
---
+++ writer.go
@@ -0,0 +1,65 @@
+package lib
+
+import (
+	"errors"
+	"golang.org/x/sync/errgroup"
+	"io"
+	"sync/atomic"
+)
+
+type Writer struct {
+	ch chan []byte    // 用于存储要写入的数据的通道
+	cl atomic.Bool    // 用于标识通道是否已关闭的原子变量
+	g  errgroup.Group // 用于管理协程的协程组
+	w  io.WriteCloser // 写入的目标
+}
+
+func NewWriter(w io.WriteCloser, bufferSize int) *Writer {
+
+	fw := &Writer{
+		ch: make(chan []byte, bufferSize),
+		cl: atomic.Bool{},
+		g:  errgroup.Group{},
+		w:  w,
+	}
+
+	fw.g.Go(fw.handle)
+
+	return fw
+}
+
+func (w *Writer) handle() (err error) {
+	for {
+		select {
+		case d, ok := <-w.ch:
+			if !ok {
+				return
+			}
+			if _, err = w.w.Write(d); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (w *Writer) Write(p []byte) (n int, err error) {
+	if p == nil {
+		return
+	}
+	if w.cl.Load() {
+		return 0, errors.New("channel is closed") // 如果通道已关闭，则返回错误信息
+	}
+	s := string(p)
+	select {
+	case w.ch <- []byte(s): // 将数据写入通道中
+		return len(p), nil
+	default:
+		return 0, errors.New("channel is full")
+	}
+}
+
+func (w *Writer) Close() error {
+	w.cl.Store(true)
+	close(w.ch)
+	return w.g.Wait()
+}
